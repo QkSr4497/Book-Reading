419a0fb4274ad64fc43a9f5b13ac6511769ded45
qksr1177 <ykechin@campus.haifa.ac.il>
2019-05-12 16:30:46 +0300

    * minor updates to app.js

--- src/app.js
+++ src/app.js
@@ -44,6 +44,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //==================================================================
 
 
+app.get('/', function (req, res) {
+    // the pool with emit an error on behalf of any idle clients
+    // it contains if a backend error or network partition happens
+    pool.on('error', (err, client) => {
+        console.error('Unexpected error on idle client', err)
+        process.exit(-1)
+    })
+
+    // callback - checkout a client
+    pool.connect((err, client, done) => {
+        if (err) throw err
+        client.query('SELECT * FROM "Book"', (error, result) => {
+            if (error) {
+                console.log(error.stack);
+            } else {
+                done();
+                // res.redirect('/signUp.html');
+                res.render('kid-page', { "Book": result.rows });
+            }
+        });
+        
+    });
+
+});
+
+
 
 app.get('/books', function (req, res) {
     // the pool with emit an error on behalf of any idle clients
