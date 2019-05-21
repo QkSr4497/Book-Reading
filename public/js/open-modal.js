6ba82f6a323cf6d7f3598c0b7cb8ad6e33f18bf0
maram11 <khatibmaram11@gmail.com>
2019-05-21 21:32:17 +0300

    Add teacher page-------should rename to avoid conflict between kid pages and other user pages

---
+++ public/js/open-modal.js
@@ -0,0 +1,42 @@
+
+// Get the modal
+var modal = document.getElementById('addbook');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+// Get the modal
+var modal = document.getElementById('additem');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('newquiz');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('newgroup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
