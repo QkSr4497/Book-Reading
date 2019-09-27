c416e8b1d4fac2efcaa34cdfc9e1bac61896b64b
maram11 <khatibmaram11@gmail.com>
2019-09-27 16:46:42 +0300

    kid quizes style update, adding searching in Quizes not done and done

---
+++ public/js/kid/quizes.js
@@ -0,0 +1,39 @@
+function searchQuiz() {
+    // Declare variables
+    var input, filter, ul, li, p, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("quizUL");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      p = li[i].getElementsByTagName("p")[0];
+      txtValue = p.textContent || p.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+  
+  function searchDoneQuiz() {
+    // Declare variables
+    var input, filter, ul, li, p, i, txtValue;
+    input = document.getElementById('searchInputDoneQuiz');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("doneQuizUL");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      p = li[i].getElementsByTagName("p")[0];
+      txtValue = p.textContent || p.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
